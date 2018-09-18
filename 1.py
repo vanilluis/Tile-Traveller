0136d3bd238a63070dfadc71379f049addf09d0c
vanilluis <kjartanorn99@gmail.com>
2018-09-18 16:26:58 +0000

    Implementation 1

--- 1.py
+++ 1.py
@@ -1,20 +1,21 @@
 ''' Búum til 4 áttir North, South, East og West, og gerum notenda 
-kleypt að slá inn áttina með litlum og stórum staf.   '''
+kleypt að slá inn áttina með litlum og stórum staf.   
+https://github.com/vanilluis/Tile-Traveller/blob/master/1.py'''
 
 for_low = ""
 x = 1
 y = 1
-
+p = True
 n = "(N)orth"
 s = "(S)outh"
 e = "(E)ast"
 w = "(W)est"
 
-
 while True:
 
+
     if y == 1:
-        change = n
+        change = n + "."
         for_low = "n"
     if x == 1 and y == 2:
         change = n + " or " + e + " or " + s + "."
@@ -26,30 +27,26 @@ while True:
         change = e + " or " + s + "."
         for_low = "es"
     if x == 2 and y == 3:
-        change = e + "."
-        for_low = "e"
+        change = e + " or " + w + "."
+        for_low = "we"
     if x == 3 and y == 3:
         change = s + " or " + w + "."
         for_low = "sw"
     if x == 3 and y == 2:
-        change = s + " or " + n + "."
+        change = n + " or " + s + "."
         for_low = "sn"
     
     
-
-    print("You can travel: " + change)
+    if p == True:
+        print("You can travel: " + change)
+    p = False
     direct = str(input("Direction: "))
     low = direct.lower()
-    p = False
+
     for l in for_low:
         if low == l:
             p = True
     
-        '''elif low != l:
-            print("Not a valid direction!")
-            p = False'''
-            
-
     if p == False:
         print("Not a valid direction!")   
 
@@ -65,12 +62,6 @@ while True:
     
         if low == "w":
             x -= 1
-    p = True
-
-    if direct == "q":
-        break
-
-    print(x,y)
 
     if x == 3 and y == 1:
         break
